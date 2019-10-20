fdba76301d7dd12209259755da03f2ab674a1197
Tom-Jerry <1220774594@qq.com>
2019-10-20 21:48:22 +0800

    additional commit

--- exercise.py
+++ exercise.py
@@ -1,4 +1,6 @@
 # -*-coding: utf-8 -*-
+'''
+q1
 friend = {
     'first_name': 'yiman',
     'last_name': 'huang',
@@ -23,3 +25,107 @@ for name, num in favorite_nums.items():
     print('key:',name, 'value:', num)
 for key, value in words.items():
     print('key:',key, '\nvalue:', value)
+
+q2
+people = {
+    'LiKexin':{
+        'time':2012,
+        'result':'fail',
+    },
+
+    'WangXiaoyi':{
+        'time':2013,
+        'result':'fail',
+    },
+
+    'HuangYiman':{
+        'time':2015,
+        'result':'win',
+    },
+}
+
+for person, info in people.items():
+    print('\n\t', person)
+    for time, result in info.items():
+        print('\t time:', time)
+        print('\t result', result)
+
+q3
+pets = {
+    'melie':{
+        'type':'hasky',
+        'owner':'german'
+    },
+    'rubert':{
+        'type':'alaska',
+        'owner':'german',
+    },
+    'xizhu':{
+        'type':'samayh',
+        'owner':'orange',
+    },
+}
+
+for name, info in pets.items():
+    print('\n\tname ', name)
+    print('\ttype:',info['type'])
+    print('\towner:',info['owner'])
+
+
+#q4
+favorite_places = {
+    'fmr':['BeiJing', 'ChongQing', 'Chengdu'],
+    'hym':['Chengdu', 'ChongQing'],
+    'lkx':['BeiJing','Bristol'],
+}
+for name, places in favorite_places.items():
+    print("\n\t" + name + "'s favorite places are:")
+    for it in places:
+        print("\t" + it)
+
+#q5
+cities = {
+    'BeiJing':{
+        'country':'china',
+        'population':14,
+        'fact':'capital',
+    },
+    'Bristol':{
+        'country':'England',
+        'population':0.1,
+        'fact':'capital',
+    },
+    'Tokyo':{
+        'country':'Japan',
+        'population':0.1,
+        'fact':'capital',
+    },
+}
+
+for name, city in cities.items():
+    print("\nname: " + name)
+    print("city's location: " + city['country'])
+    print("city's population: " + str(city['population']))
+    print("city's fact: " + city['fact'])
+
+prompt = "Please tell me what ingredents you want: "
+completed = False
+while completed ==  False:
+    add = input(prompt)
+    if add == "quit":
+        completed = True
+    else:
+        print(add + "has been added!")
+
+while True:
+    age = input("\nquit to exit\nPlease enter your age: ")
+    if age == "quit":
+        break
+    age = int(age)
+    if age < 3:
+        print("\nYou don't need to pay any coins")
+    elif age >=3 and age <= 12:
+        print("\nYou need to pay 10 dollars")
+    else:
+        print("\nYou need to pay 15 dollars")
+'''
