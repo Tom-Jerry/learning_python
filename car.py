756ae80505b2b7f1c9e1386cce140e0cc425daac
Tom-Jerry <1220774594@qq.com>
2019-10-24 20:57:46 +0800

    1024 update

--- car.py
+++ car.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 class Car():
-    """ 汽车基类"""
+    """一次简单模拟汽车的尝试"""
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
@@ -11,42 +11,34 @@ class Car():
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
     def read_odometer(self):
-        print("This car has " + str(self.odometer_reading) +
-        " miles on it.")
-    def update_odometer(self, mileage):
+        print("The car has " + str(self.odometer_reading) + " miles on it")
+
+    def update_odometer(self,mileage):
         if mileage >= self.odometer_reading:
             self.odometer_reading = mileage
         else:
-            print("You can't roll back an odometer!")
+            print("You can't roll back odometer!")
     def increment_odometer(self, miles):
         self.odometer_reading += miles
-    
-    def fill_gas_tank(self):
-        print("Now you've get your tank filed!")
+
 
 class Battery():
-    def __init__(self, battery_size = 70):
+    """模拟汽车电瓶"""
+    def __init__(self, battery_size=70):
         self.battery_size = battery_size
     def describe_battery(self):
-        print("This car has a " + str(self.battery_size) + " -KWh battery.")
+        print("This car has a " + str(self.battery_size) + "-KWh battery.")
+
     def get_range(self):
         if self.battery_size == 70:
             range = 240
         elif self.battery_size == 85:
             range = 270
-        message = "This car can go approximately "  + str(range)
+        message = "This car can go approximately " + str(range)
         message += " miles on a full charge."
         print(message)
-
 
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
         self.battery = Battery()
-
-    def fill_gas_tank(self):
-        print("There is no tank in electric car!")
-my_tesla = ElectricCar('tesla', 'mode s', 2016)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
--- exercise.py
+++ exercise.py
@@ -235,4 +235,114 @@ def make_car(producer, num, **others):
 
 new_car = make_car('subaru', 'outback', color='blue', package='two')
 print(new_car)
+
+class Restaurant():
+    def __init__(self, restaurant_name, restaurant_type,
+        number_served=0):
+        self.restaurant_name =  restaurant_name
+        self.restaurant_type =  restaurant_type
+        self.number_served = number_served
+    
+    def set_number_served(self,value):
+        self.number_served = value
+    
+    def increment_number_served(self,increment):
+        self.number_served += increment
+
+    def describe_restaurant(self):
+        print("\nRestaurant name: " + self.restaurant_name)
+        print("Restaurant type: " + self.restaurant_type)
+        print("Restaurant has served " + str(self.number_served) + " persons")
+    def open_restaurant(self):
+        print("\nNow the restaurant is on !")
+
+class IceCreamStand(Restaurant):
+    def __init__(self,restaurant_name, restaurant_type,number_served=0):
+        super().__init__(restaurant_name, restaurant_type, number_served)
+        self.flavors = ['lemon', 'milk', 'orange']
+    def describe_icecream(self):
+        print("The tastes of our icecream: " + str(self.flavors))
+ 
+class User():
+    def __init__(self, first_name, last_name,login_attempts):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login_attempts = login_attempts
+    
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print("\nuser info " + self.first_name + " " + self.last_name +".")
+        print("login attempts: " + str(self.login_attempts) + '.')
+
+    def greet_user(self):
+        print("\nhello " + self.first_name + " " + self.last_name + ".")
+
+class Admin(User):
+    def __init__(self,first_name, last_name, login_attempts):
+         super().__init__(first_name,last_name,login_attempts)
+         self.privileges = 'can ban user'
+    def show_privileges(self):
+        print(self.privileges) 
+ 
 '''
+
+
+class Car():
+    """ 汽车基类"""
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) +
+        " miles on it.")
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+    
+    def fill_gas_tank(self):
+        print("Now you've get your tank filed!")
+
+class Battery():
+    def __init__(self, battery_size = 70):
+        self.battery_size = battery_size
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + " -KWh battery.")
+    def upgrade_battery(self):
+        if self.battery_size != 85:
+            self.battery_size = 85
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately "  + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        print("There is no tank in electric car!")
+
+my_tesla = ElectricCar('tesla', 'mode s', 2019)
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
